8e8a60856a700ce4598af6169a8e3eecc633eed2
ChesterD02 <chesterd02@gmail.com>
2021-03-17 17:39:11 -0600

    added parse_csv with query

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import dns.dnssec
 import dns.message
 import dns.resolver
 import dns.rdatatype
+import csv
 
-def query():
+def query(domain):
     # get nameservers for target domain
-    response = dns.resolver.resolve('com.', dns.rdatatype.NS)
+    response = dns.resolver.resolve(domain, dns.rdatatype.NS)
 
     # we'll use the first nameserver in this example
     nsname = response.rrset[0].to_text()  # name
@@ -15,34 +16,65 @@ def query():
     nsaddr = response.rrset[0].to_text()  # IPv4
 
     # get DNSKEY for zone
-    request = dns.message.make_query('com.',
+    request = dns.message.make_query(domain,
                                      dns.rdatatype.DNSKEY,
                                      want_dnssec=True)
     # print("request: ", request)
 
     # send the query
-    response = dns.query.udp(request, nsaddr)
+    try:
+        response = dns.query.udp(request, nsaddr, timeout=3)
+    except:
+        return False
     if response.rcode() != 0:
         print("HANDLE QUERY FAILED - SERVER ERROR OR NO DNSKEY RECORD")
 
     # answer should contain two RRSET: DNSKEY and RRSIG(DNSKEY)
+
     answer = response.answer
-    print("answer: ", answer)
+    # print("answer: ", answer)
     if len(answer) != 2:
-        print("SOMETHING WENT WRONG THE ANSWER SHOULD HAVE 2 THINGS IN IT")
+        #print("SOMETHING WENT WRONG THE ANSWER SHOULD HAVE 2 THINGS IN IT")
+        return False
 
     # the DNSKEY should be self signed, validate it
-    name = dns.name.from_text('com.')
+    name = dns.name.from_text(domain)
 
     try:
-        valid = dns.dnssec.validate(answer[0], answer[1], {name: answer[0]})
+        dns.dnssec.validate(answer[0], answer[1], {name: answer[0]})
     except dns.dnssec.ValidationFailure:
         print("BE SUSPICIOUS THIS DNSKEY IS NOT SELF SIGNED")
+        return False
     else:
-        print("WE'RE GOOD, THERE'S A VALID DNSSEC SELF-SIGNED KEY FOR the query")
-        print("DATA: ", valid)
+        print("WE'RE GOOD, THERE'S A VALID DNSSEC SELF-SIGNED KEY FOR THE QUERY")
+        return True
+
+def parse_csv():
+    with open('top-1m.csv', newline='') as csvfile:
+        dialect = csv.Sniffer().sniff(csvfile.read(1024))
+        csvfile.seek(0)
+        reader = csv.reader(csvfile, dialect)
+        test_number_to_parse = 100                     # This number is used for testing on a smaller set of data
+        domain = next(reader)
+
+        # for row in reader:
+        #     do stuff with row
+        response = []
+
+        while int(domain[0]) != test_number_to_parse:
+            # print ("line: ", domain[0])
+            response.append(query(domain[1]))
+            domain = next(reader)
+        return response
+
 
 if __name__ == '__main__':
-    query()
+    list = parse_csv()
+    total = sum(1 for x in list if x==True)
+    print ("List: ", list)
+    print ("Total DNSSEC: ", total)
+
+
+
 
 
