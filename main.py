fe48fc4d5345b98960f6d1de5a7b642cd16945d8
ChesterD02 <chesterd02@gmail.com>
2021-03-31 12:30:09 -0600

    cleaned data ready for sqlite import

--- main.py
+++ main.py
@@ -7,69 +7,99 @@ import dns.rdatatype
 import csv
 import re
 import json
+import pickle
 
-def analyzeAlgorithm(answer):
-    print (answer)
-    for RRSet in answer:
-        print (json.dumps(RRSet))
-        # x = re.search(, str(RRSet))
-        print (RRSet.items['rdata'])
-        #https://dnspython.readthedocs.io/en/stable/dnssec.html
-        # algorithm_to_text?
-        if (RRSet.items["rdata"] != None):
-            if RRSet.items["rdata"].contains("13"):
-                print("algorithm 13")
-        else: print("error")
-    return
+class myObject:
+    def __init__(self,
+                 domain,
+                 ds_response,
+                 soa_response,
+                 a_response,
+                 ns_response,
+                 response,
+                 ds_id,
+                 answer,
+                 algorithm_number,
+                 algorithm,
+                 key_id):
+        self.domain = domain
+        self.ds_response = ds_response
+        self.soa_response = soa_response
+        self.a_response = a_response
+        self.ns_response = ns_response
+        self.response = response
+        self.ds_id = ds_id
+        self.answer = answer
+        self.algorithm_number = algorithm_number
+        self.algorithm = algorithm
+        self.key_id = key_id
 
-def query(domain):
-    # get nameservers for target domain
-    response = dns.resolver.resolve(domain, dns.rdatatype.NS)
+def getquerydata(domain):
+    domain = 'salesforce.com'
+    ds_response = dns.resolver.resolve(domain, dns.rdatatype.DS)
+    soa_response = dns.resolver.resolve(domain, dns.rdatatype.SOA)
+    a_response = dns.resolver.resolve(domain, dns.rdatatype.A)
+    ns_response = dns.resolver.resolve(domain, dns.rdatatype.NS)
 
-    # we'll use the first nameserver in this example
-    nsname = response.rrset[0].to_text()  # name
+    nsname = ns_response.rrset[0].to_text()
+    ds_id = str.split(ds_response.rrset[0].to_text(),' ')[0]
+
     response = dns.resolver.resolve(nsname, dns.rdatatype.A)
     nsaddr = response.rrset[0].to_text()  # IPv4
 
     # get DNSKEY for zone
-    request = dns.message.make_query(domain,
-                                     dns.rdatatype.DNSKEY,
-                                     want_dnssec=True)
-    # print("request: ", request)
-
-    # send the query
+    request = dns.message.make_query(domain, dns.rdatatype.DNSKEY, want_dnssec=True)
     try:
-        response = dns.query.udp(request, nsaddr, timeout=3)
+        response = dns.query.udp(request, nsaddr, timeout=10)
     except:
-        return False
-    if response.rcode() != 0:
-        print("HANDLE QUERY FAILED - SERVER ERROR OR NO DNSKEY RECORD")
-
-    # answer should contain two RRSET: DNSKEY and RRSIG(DNSKEY)
+        #No DNSSEC data
+        return None
 
     answer = response.answer
-    print("answer: ", answer)
-    # 257 is the key signing key (KSK)
-    # 256 is the ZSK
-
+    name = dns.name.from_text(domain)
     if len(answer) != 2:
         #print("SOMETHING WENT WRONG THE ANSWER SHOULD HAVE 2 THINGS IN IT")
-        return False
-
-    # Look at the algorithm being used
-    analyzeAlgorithm(answer)
+        return None
 
     # the DNSKEY should be self signed, validate it
-    name = dns.name.from_text(domain)
-
     try:
         dns.dnssec.validate(answer[0], answer[1], {name: answer[0]})
     except dns.dnssec.ValidationFailure:
         print("BE SUSPICIOUS THIS DNSKEY IS NOT SELF SIGNED")
-        return False
-    else:
-        print("WE'RE GOOD, THERE'S A VALID DNSSEC SELF-SIGNED KEY FOR THE QUERY")
-        return True
+        return None
+    print("WE'RE GOOD, THERE'S A VALID DNSSEC SELF-SIGNED KEY FOR THE QUERY")
+
+    keys = {name: answer[0]}
+    for rrsigset in answer[1]:
+        if isinstance(rrsigset, tuple):
+            rrsigrdataset = rrsigset[1]
+        else:
+            rrsigrdataset = rrsigset
+
+        algorithm_number = int(str.split(rrsigrdataset.to_text(), ' ')[1])
+        algorithm = dns.dnssec.algorithm_to_text(algorithm_number)
+
+        dns.dnssec.validate_rrsig(answer[0], rrsigrdataset, keys)
+        candidate_key = dns.dnssec._find_candidate_keys(keys, rrsigrdataset)
+        key_id = dns.dnssec.key_id(candidate_key[0])
+        print ("KEYID****: ", key_id)
+
+    #return an object to be put in a sqlite table?
+    newObject = myObject(domain,
+                        ds_response,
+                        soa_response,
+                        a_response,
+                        ns_response,
+                        response,
+                        ds_id,
+                        answer,
+                        algorithm_number,
+                        algorithm,
+                        key_id)
+    picklestring = pickle.dumps(newObject)
+    # unpickle = pickle.loads(picklestring)
+
+    return picklestring
 
 def parse_csv():
     with open('top-1m.csv', newline='') as csvfile:
@@ -87,7 +117,7 @@ def parse_csv():
             if int(domain[0]) < 40:
                 domain = next(reader)
                 continue
-            response.append(query(domain[1]))
+            response.append(getquerydata(domain[1]))
             domain = next(reader)
         return response
 
@@ -99,6 +129,31 @@ if __name__ == '__main__':
     print ("Total DNSSEC: ", total)
 
 
+
+class myObject:
+    def __init__(self,
+                 domain=None,
+                 ds_response=None,
+                 soa_response=None,
+                 a_response=None,
+                 ns_response=None,
+                 response=None,
+                 ds_id=None,
+                 answer=None,
+                 algorithm_number=None,
+                 algorithm=None,
+                 key_id=None):
+        self.domain = domain
+        self.ds_response = ds_response
+        self.soa_response = soa_response
+        self.a_response = a_response
+        self.ns_response = ns_response
+        self.response = response
+        self.ds_id = ds_id
+        self.answer = answer
+        self.algorithm_number = algorithm_number
+        self.algorithm = algorithm
+        self.key_id = key_id
 
 
 
